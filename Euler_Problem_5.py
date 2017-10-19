147d4022ed7403764c9e2c39294cd178621db8bc
Shivakumar Mahakali <mahakalim@yahoo.com>
2017-10-19 18:19:26 -0400

    Added Project Euler Problem number 5.

---
+++ Euler_Problem_5.py
@@ -0,0 +1,34 @@
+# Script Name       : Euler_Problem_5.py
+# Author            : Shivakumar Mahakali
+#
+# Description       : Solution to Problem 5 from Project Euler.
+#
+# Problem 1         : 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without
+#                     any remainder. What is the smallest positive number that is evenly divisible by all of the
+#                     numbers from 1 to 20?
+
+
+def smallest_number(limit):
+    """
+    calculates the smallest number that can be evenly divisible by all numbers from 1 to limit.
+    :param limit: Upper limit.
+    :return: [int] Smallest number.
+    """
+    number_list = []
+    for i in range(1, limit + 1):
+        number_list.append(i)
+
+    solution = []
+    for number in range(limit, 999999999, limit):
+        if all(number % num == 0 for num in number_list):
+            solution.append(number)
+        try:
+            if solution[0] is not None:
+                break
+        except IndexError:
+            continue
+
+    return min(solution)
+
+
+print(smallest_number(20))
